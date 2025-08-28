f7ab5b8026803fd9d054e77a7983dbcf610c05f5
IkhwanVanno <ikhwanvanno750@gmail.com>
2025-08-28 16:24:27 +0700

    fix timezone

--- app/_Config.php
+++ app/_Config.php
@@ -5,3 +5,4 @@ use SilverStripe\SiteConfig\SiteConfig;
 
 SiteConfig::add_extension(CustomSiteConfig::class);
 Member::add_extension(MemberExtension::class);
+date_default_timezone_set('Asia/Jakarta');
